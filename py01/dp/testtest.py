1e122339997d8b0112ca17c4dcee77b4d65e19a5
mrli008 <952934650@qq.com>
2017-01-07 20:46:58 +0800

    动规练习 整数01背包问题 凑硬币问题

---
+++ py01/dp/testtest.py
@@ -0,0 +1,6 @@
+# coding=utf8
+
+length1 = 10
+length2 = 10
+
+print len([(x,y) for x in range(length1) for y in range(length2)])
--- py01/dp/zuishaoyingbi.py
+++ py01/dp/zuishaoyingbi.py
@@ -16,14 +16,53 @@ output:
 import numpy as np
 
 
+max_value = 1000000
+
 def solve(num, left_money, array_money, result):
     if left_money == 0:
-        return num
+        return 0
     if left_money < 0:
-        return 1000000000
+        return max_value
+    if num >= len(array_money):
+        return max_value
+    if result[num, left_money] > 0:
+        return result[num, left_money]
 
-    return min([solve(num + 1, left_money - money, array_money, result)  for money in array_money])
+    result[num, left_money] = min(solve(num, left_money - array_money[num], array_money, result)+1,
+               solve(num+1, left_money, array_money, result))
+    return result[num, left_money]
 
+    # return [solve(num+1, left_money - money, array_money)  for money in array_money]
+
+def solve_not_recursion(total_money, array_money, result):
+    result[0,0] = 0
+
+    for i in range(len(array_money)):
+        result[i,0] = 0
+        j=1
+        while j <= total_money:
+            result[i, j] = result[i-1, j]
+            if j >= array_money[i]:
+                result[i, j] = min(result[i, j-array_money[i]] + 1,
+                                   result[i + 1, j])
+            j += 1
+        print result
+        i += 1
+
+    return result[i-1, total_money]
+
+
+def find_true_from_list(l):
+    if isinstance(l, list):
+        print 'list'
+        for v in l:
+            find_true_from_list(v)
+    elif isinstance(l, dict):
+        print 'dict', l
+        if True in l.keys():
+            return l.get(True)
+    else:
+        print 'other type'
 
 
 
@@ -42,8 +81,19 @@ def solve_my(values, total_money, result):
 
 
 if __name__ == '__main__':
-    value = [ 2, 5]
-    total_money = 11
-    print solve(0, total_money, value, None)
+    value = [ 1,2, 5]
+    total_money = 130
+    result = np.zeros((len(value)+1, total_money+1), dtype=np.int)
+    print u'金币的个数: ', solve(0, total_money, value, result)
+    print result
+    # issuccess = find_true_from_list(result)
+    # if issuccess is None:
+    #     print -1
+    # else:
+    #     print issuccess
     result = list()
     print solve_my(value, total_money, result)
+
+    result = np.zeros((len(value)+1, total_money+1), dtype=np.int)
+    result[:] = max_value
+    print solve_not_recursion(total_money, value, result)
