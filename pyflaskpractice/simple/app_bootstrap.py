f3282ef9d9680d4434c19a7fe89ab5a51502afe3
mrli008 <952934650@qq.com>
2017-01-09 14:46:14 +0800

    动规练习 整数01背包问题 凑硬币问题 pandas/Series, DataFrame的学习 着手建设web, 使用flask 添加网站图标 添加moment.js 使用extends实现页面内容的继承 flash

--- pyflaskpractice/simple/app_bootstrap.py
+++ pyflaskpractice/simple/app_bootstrap.py
@@ -1,6 +1,6 @@
 # coding=utf8
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, url_for, flash
 
 from flask_script import Manager
 
@@ -9,12 +9,25 @@ from flask_moment import Moment
 
 from datetime import datetime
 
+# form
+from flask_wtf.form import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name:', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+
 
 
 app = Flask(__name__)
 # manage = Manager(app)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
+
+
 
 @app.route('/')
 def index():
@@ -36,6 +49,24 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+@app.route('/get', methods=['GET', 'POST'])
+def get_data():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        print 'your old input: ', old_name, form.name.data
+        if old_name is not None and old_name != form.name.data:
+            print 'it is not same'
+
+            flash('Looks like you have changed your name!')
+
+        session['name'] = form.name.data
+        return redirect(url_for('get_data'))
+
+    return render_template('get.html', form=form, name=session.get('name'))
+
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 
 if __name__ == '__main__':
