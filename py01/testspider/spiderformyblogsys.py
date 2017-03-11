1c794802f31dacb7bc747093e1ede744b04efadc
mrli008 <952934650@qq.com>
2017-03-12 00:01:01 +0800

    七月在线
    网络数据采集
    requests和beautifulsoup
    scikit-learn
    
    通过同一session登录

--- py01/testspider/spiderformyblogsys.py
+++ py01/testspider/spiderformyblogsys.py
@@ -2,31 +2,59 @@
 
 
 import requests as req
+import BeautifulSoup as bs
+import codecs
 
 
 
 
-def savedata(filename, filedata):
-    with open(filename, 'w') as f:
+def savedata(filename, filedata, encoding):
+    with codecs.open(filename=filename, mode='w', encoding=encoding) as f:
         f.write(filedata)
 
+def confirmencoding(content):
+    encoding = req.utils.get_encodings_from_content(content)
+    if encoding in (None, []):
+        return 'utf-8'
+    return encoding[0]
 
 
-url = "http://127.0.0.1:5000/auth/login"
-data={'email':'952934650@qq.com',
-      'password':'123456'}
 
-urldata = req.post(url, data=data)
+url = "http://192.168.1.106:801/auth/login"
+# data={'email':'952934650@qq.com',
+#       'password':'123456'}
 
+# urldata = req.post(url, data=data)
+s = req.session()
+urldata = s.get(url)
+print '....'
+print '....'
 encoding = req.utils.get_encodings_from_content(urldata.content)
 
-print encoding
+urldata.encoding = confirmencoding(urldata.content)
 
-if encoding in (None, []):
-    encoding = 'utf-8'
-else:
-    encoding = encoding[0]
+# savedata(filename='logindata.html', filedata=urldata.text)
+# print urldata.text
 
-urldata.encoding = encoding
 
-savedata(filename='logindata.html', filedata=urldata.text)
+
+urlbs = bs.BeautifulSoup(urldata.text)
+
+postdata = dict()
+
+csrf_token = urlbs.find('input', {'id':'csrf_token','name':'csrf_token'})
+
+postdata['csrf_token'] = csrf_token['value']
+postdata['email'] = '952934650@qq.com'
+postdata['password'] = '123456'
+
+print postdata
+
+urldata = s.post(url, data=postdata)
+
+
+urldata.encoding = confirmencoding(urldata.content)
+
+savedata(filename='logindatawithcsrf.html', filedata=urldata.text, encoding=urldata.encoding)
+
+
