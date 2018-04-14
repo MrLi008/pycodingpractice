ee5a91b3ecea6472e181492f6cd18814f85db6f5
mrli008 <952934650@qq.com>
2018-04-14 21:02:58 +0800

    ctfprac

--- py01/ctfprac/prac1.py
+++ py01/ctfprac/prac1.py
@@ -9,18 +9,48 @@ sess = requests.session()
 url = 'http://ctf5.shiyanbar.com/web/jiandan/index.php'
 response = sess.get(url)
 print (response)
-headers = response.headers
-cookies = response.cookies
+headers = sess.headers
+cookies = sess.cookies
+
+print (headers)
+print (cookies)
+
 
 data = {
-    'id':'id','submit':'submit'
+    'id':'1;%00','submit':'Login'
 }
 
 response = sess.post(url,data=data,headers=headers,cookies=cookies)
 print (response)
-print (response.text)
-print (response.headers)
-print (response.cookies)
+print (response.content)
+
+headers = sess.headers
+cookies = sess.cookies
+print (headers)
+print (cookies)
+data = {
+    'submit':'Login'
+}
+response = sess.post(url, data=data,headers=headers,cookies=cookies)
+print (response)
+print (response.content)
+
+headers = response.headers
+cookies = response.cookies
+print (headers)
+print (cookies)
+
+data = {
+     'submit':'Login'
+}
+response = sess.post(url, data=data,headers=headers,cookies=cookies)
+print (response)
+print (response.content)
+
+headers = response.headers
+cookies = response.cookies
+print (headers)
+print (cookies)
 
 
 if __name__ == '__main__':
