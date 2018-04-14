2fe3136bc2176fd70e65912085e1b9150db7755f
mrli008 <952934650@qq.com>
2018-04-14 21:25:47 +0800

    ctfprac

--- py01/ctfprac/prac2.py
+++ py01/ctfprac/prac2.py
@@ -4,6 +4,30 @@
     :date  
     :funcname
 '''
+import requests
+sess = requests.session()
+url = 'http://ctf5.shiyanbar.com/423/web/?id='
+response = sess.post(url)
+
+headers = sess.headers
+cookies = sess.cookies
+print (response)
+print (response.content)
+print (headers)
+print (cookies)
+
+
+response = sess.get(url+'\'%00')
+
+headers = sess.headers
+cookies = sess.cookies
+print (response)
+print (response.content)
+print (headers)
+print (cookies)
+data = {
+    'id':1
+}
 
 if __name__ == '__main__':
     pass
